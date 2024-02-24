c1ae9bb66b21e7916148cf0d7658ac3f70c2968c
BryanIvander <s160420137@student.ubaya.ac.id>
2024-02-24 09:59:18 +0700

    branch Activity Add

---
+++ app/src/main/java/com/example/anmp160420137/BranchActivity.kt
@@ -0,0 +1,11 @@
+package com.example.anmp160420137
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BranchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_branch)
+    }
+}
