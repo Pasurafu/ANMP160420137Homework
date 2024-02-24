7686ccc606d4195467aae93e9ea0043bce2f62a9
BryanIvander <s160420137@student.ubaya.ac.id>
2024-02-24 08:50:59 +0700

    week1 Activity Add

---
+++ app/src/main/java/com/example/anmp160420137/ActivityWeek1.kt
@@ -0,0 +1,11 @@
+package com.example.anmp160420137
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityWeek1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_week1)
+    }
+}
